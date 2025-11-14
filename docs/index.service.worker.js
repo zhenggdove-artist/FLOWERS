8bdd06ed9ead707af85be6850720853afdd0af2c
zhenggdove-artist <zhenggdove@gmail.com>
2025-11-15 03:16:08 +0800

    update5

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763147006|14815372833';
+const CACHE_VERSION = '1763147756|15565466253';
 /** @type {string} */
 const CACHE_PREFIX = '殖民之花-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
